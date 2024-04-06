7f0256c8d02e52b0e6458024288b2d6bc3098c6b
LEVIS <levisrabah@gmail.com>
2024-04-06 15:39:36 +0300

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('%c HI', 'color: firebrick')
+
+//url fetching images of differnt breeds of dogs
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+//Url fetching dog breeds
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
